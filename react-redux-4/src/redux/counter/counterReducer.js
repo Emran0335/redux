38ffbbfe987a16aaae9b1cf27afff22af3c9d153
Emran Hossain <emranhossain0335@gmail.com>
2023-08-05 12:50:34 +0600

    redux

---
+++ react-redux-4/src/redux/counter/counterReducer.js
@@ -0,0 +1,24 @@
+import { DECREMENT, INCREMENT } from "./actions";
+
+const initialState = {
+  value: 0,
+};
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        value: state.value + action.payload,
+      };
+    case DECREMENT:
+      return {
+        ...state,
+        value: state.value + action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default counterReducer;
---
+++ react-redux-4/src/redux/store.js
@@ -0,0 +1,7 @@
+import { createStore } from "redux";
+import counterReducer from "./counter/counterReducer";
+
+
+const store = createStore(counterReducer)
+
+export default store;
