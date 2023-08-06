ada281afbe8c4411b0cdae782b0ed10067c3cc51
Emran Hossain <emranhossain0335@gmail.com>
2023-08-06 20:34:02 +0600

    redux

---
+++ react-redux-5/src/redux/store.js
@@ -0,0 +1,17 @@
+import { applyMiddleware, createStore } from "redux";
+import rootReducer from "./rootReducer";
+
+// create our first middleware
+const myLogger = (store) => (next) => (action) => {
+  console.log(`Action: ${JSON.stringify(action)}`);
+  console.log(`Before: ${JSON.stringify(store.getState())}`);
+  // toCheck upcoming value
+  const upComingState = [action].reduce(rootReducer, store.getState())
+  console.log(`Upcoming State: ${JSON.stringify(upComingState)}`)
+  // pass action
+  return next(action)
+};
+
+const store = createStore(rootReducer, applyMiddleware(myLogger));
+
+export default store;
