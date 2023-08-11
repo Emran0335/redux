4b0c336b9e4828e791577dca8382e26ebfd76023
Emran Hossain <emranhossain0335@gmail.com>
2023-08-11 21:21:33 +0600

    redux

--- react-redux-app-1/src/redux/filters/filterReducer.js
+++ react-redux-app-1/src/redux/filters/filterReducer.js
@@ -14,12 +14,14 @@ const filterReducer = (state = initailState, action) => {
         case "added":
           return {
             ...state,
-            colors: [...state.colors, color]
+            colors: [...state.colors, color],
           };
         case "removed":
           return {
             ...state,
-            colors: state.colors.filter((existingColor) => existingColor !== color),
+            colors: state.colors.filter(
+              (existingColor) => existingColor !== color
+            ),
           };
         default:
           return state;
@@ -29,3 +31,77 @@ const filterReducer = (state = initailState, action) => {
   }
 };
 export default filterReducer;
+
+/*
+// filter reducer
+const filterInitialState = {
+  status: "All",
+  colors: [],
+};
+
+const statusChanged = (type, payload) => {
+  return {
+    type: type,
+    payload: payload,
+  };
+};
+
+const filterReducerStatusAction = (state, action) => {
+  switch (action.type) {
+    case "changeStatus":
+      return {
+        ...state,
+        status: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+const filterStatusActionResult = filterReducerStatusAction(
+  filterInitialState,
+  statusChanged("changeStatus", "Complete")
+);
+console.log("filterStatusActionResult", filterStatusActionResult);
+
+const colorChanged = (type, color, changedType) => {
+  return {
+    type: type,
+    payload: {
+      color: color,
+      changedType: changedType,
+    },
+  };
+};
+
+const filterReducerColorChanged = (state, action) => {
+  switch (action.type) {
+    case "changedColor":
+      const { color, changedType } = action.payload;
+      switch (changedType) {
+        case "added":
+          return {
+            ...state,
+            colors: [...state.colors, color],
+          };
+        case "removed":
+          return {
+            ...state,
+            colors: state.colors.filter(
+              (existingColor) => existingColor !== color
+            ),
+          };
+        default:
+          return state;
+      }
+    default:
+      return state;
+  }
+};
+
+const filterColorChangedResult = filterReducerColorChanged(
+  filterInitialState,
+  colorChanged("changedColor", "blue", "added")
+);
+console.log("filterColorChangedResult", filterColorChangedResult);
+*/
