b01b7ce3edcb1a37c21c95a81451bad5433873fd
Emran Hossain <emranhossain0335@gmail.com>
2023-10-19 19:43:58 +0600

    everything is done and project is getting developed

--- redux-module-7/src/app/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import transactionReducer from "../features/transaction/transactionSlice";
-export const store = configureStore({
-  reducer: {
-    transaction: transactionReducer,
-  },
-});
